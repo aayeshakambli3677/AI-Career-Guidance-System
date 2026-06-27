008530172673c2a462520eede4250e44f6c7de54
aayeshakambli3677 <aayeshakambli@gmail.com>
2026-06-27 20:14:45 +0530

    backend completed

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -2,37 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    """
-    Application configuration settings
-    """
-
-    # =========================
-    # PROJECT SETTINGS
-    # =========================
     PROJECT_NAME: str = "AI Career Guidance System"
     VERSION: str = "1.0.0"
     DEBUG: bool = True
 
-    # =========================
-    # DATABASE SETTINGS
-    # =========================
-    DATABASE_URL: str = "mongodb://localhost:27017/careergpt"
-
-    # =========================
-    # SECURITY SETTINGS
-    # =========================
-    SECRET_KEY: str = "supersecretkey"
+    DATABASE_URL: str
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
-    # =========================
-    # AI SERVICE SETTINGS
-    # =========================
-    OPENAI_API_KEY: str = ""
-
-    # =========================
-    # CORS SETTINGS
-    # =========================
     ALLOWED_ORIGINS: list[str] = [
         "http://localhost:3000",
         "http://127.0.0.1:3000"
@@ -42,5 +20,4 @@ class Settings(BaseSettings):
         env_file = ".env"
 
 
-# Global settings instance (use everywhere in project)
 settings = Settings()
--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+from app.config.settings import settings
+# import os
 
-load_dotenv()
+# load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
