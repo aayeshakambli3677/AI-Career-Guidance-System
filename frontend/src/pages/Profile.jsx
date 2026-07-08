8a64c4602c0f449fa7e61397bb231147c2a7ed5d
shraddhasatishbirajdar@2008 <sneha1234birajdar@gmil.com>
2026-07-08 17:02:48 +0530

    done

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,7 +1,38 @@
+
+import "./Profile.css";
 function Profile() {
     return (
-        <div>
-            <h1>Profile Page</h1>
+        <div className="profile-page">
+
+            <h1>My Profile</h1>
+
+            <div className="profile-card">
+
+                <div className="profile-image">
+                    <img
+                        src="/profile.png"
+                        alt="Profile"
+                    />
+                </div>
+
+                <div className="profile-details">
+
+                    <h2>Student Name</h2>
+
+                    <p>Email: student@gmail.com</p>
+
+                    <p>Education: Computer Engineering</p>
+
+                    <p>Skills: React, Python, FastAPI</p>
+
+                    <p>Career Goal: Software Developer</p>
+
+                    <button>Edit Profile</button>
+
+                </div>
+
+            </div>
+
         </div>
     );
 }
